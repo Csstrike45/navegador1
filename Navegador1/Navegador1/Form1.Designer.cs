f7b7508f3afff052858aa5801e4a034ba790be76
Csstrike45 <103398495+Csstrike45@users.noreply.github.com>
2022-07-11 20:08:26 -0400

    Add files via upload

---
+++ Navegador1/Navegador1/Form1.Designer.cs
@@ -0,0 +1,175 @@
+﻿
+namespace Navegador1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.toolStrip1 = new System.Windows.Forms.ToolStrip();
+            this.webBrowser1 = new System.Windows.Forms.WebBrowser();
+            this.btnAtras = new System.Windows.Forms.ToolStripButton();
+            this.btnAdelante = new System.Windows.Forms.ToolStripButton();
+            this.btnActualizar = new System.Windows.Forms.ToolStripButton();
+            this.btnParar = new System.Windows.Forms.ToolStripButton();
+            this.txtUrl = new System.Windows.Forms.ToolStripTextBox();
+            this.btnCasa = new System.Windows.Forms.ToolStripButton();
+            this.btnBuscar = new System.Windows.Forms.ToolStripButton();
+            this.toolStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // toolStrip1
+            // 
+            this.toolStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.btnAtras,
+            this.btnAdelante,
+            this.btnActualizar,
+            this.btnParar,
+            this.btnCasa,
+            this.txtUrl,
+            this.btnBuscar});
+            this.toolStrip1.Location = new System.Drawing.Point(0, 0);
+            this.toolStrip1.Name = "toolStrip1";
+            this.toolStrip1.Size = new System.Drawing.Size(872, 25);
+            this.toolStrip1.TabIndex = 0;
+            this.toolStrip1.Text = "toolStrip1";
+            // 
+            // webBrowser1
+            // 
+            this.webBrowser1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.webBrowser1.Location = new System.Drawing.Point(0, 25);
+            this.webBrowser1.MinimumSize = new System.Drawing.Size(20, 20);
+            this.webBrowser1.Name = "webBrowser1";
+            this.webBrowser1.Size = new System.Drawing.Size(872, 425);
+            this.webBrowser1.TabIndex = 1;
+            this.webBrowser1.Url = new System.Uri("http://www.google.com", System.UriKind.Absolute);
+            // 
+            // btnAtras
+            // 
+            this.btnAtras.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.btnAtras.Image = ((System.Drawing.Image)(resources.GetObject("btnAtras.Image")));
+            this.btnAtras.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.btnAtras.Name = "btnAtras";
+            this.btnAtras.Size = new System.Drawing.Size(23, 22);
+            this.btnAtras.Text = "toolStripButton1";
+            this.btnAtras.ToolTipText = "volver a la pagina anterior";
+            this.btnAtras.Click += new System.EventHandler(this.btnAtras_Click);
+            // 
+            // btnAdelante
+            // 
+            this.btnAdelante.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.btnAdelante.Image = ((System.Drawing.Image)(resources.GetObject("btnAdelante.Image")));
+            this.btnAdelante.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.btnAdelante.Name = "btnAdelante";
+            this.btnAdelante.Size = new System.Drawing.Size(23, 22);
+            this.btnAdelante.Text = "toolStripButton2";
+            this.btnAdelante.ToolTipText = "ir a pagina siguiente";
+            this.btnAdelante.Click += new System.EventHandler(this.btnAdelante_Click);
+            // 
+            // btnActualizar
+            // 
+            this.btnActualizar.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.btnActualizar.Image = ((System.Drawing.Image)(resources.GetObject("btnActualizar.Image")));
+            this.btnActualizar.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.btnActualizar.Name = "btnActualizar";
+            this.btnActualizar.Size = new System.Drawing.Size(23, 22);
+            this.btnActualizar.Text = "toolStripButton3";
+            this.btnActualizar.ToolTipText = "refrescar la pagina";
+            this.btnActualizar.Click += new System.EventHandler(this.btnActualizar_Click);
+            // 
+            // btnParar
+            // 
+            this.btnParar.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.btnParar.Image = ((System.Drawing.Image)(resources.GetObject("btnParar.Image")));
+            this.btnParar.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.btnParar.Name = "btnParar";
+            this.btnParar.Size = new System.Drawing.Size(23, 22);
+            this.btnParar.Text = "toolStripButton4";
+            this.btnParar.ToolTipText = "detener la carga";
+            this.btnParar.Click += new System.EventHandler(this.btnParar_Click);
+            // 
+            // txtUrl
+            // 
+            this.txtUrl.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.txtUrl.Name = "txtUrl";
+            this.txtUrl.Size = new System.Drawing.Size(600, 25);
+            this.txtUrl.ToolTipText = "barra de busqueda";
+            // 
+            // btnCasa
+            // 
+            this.btnCasa.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.btnCasa.Image = ((System.Drawing.Image)(resources.GetObject("btnCasa.Image")));
+            this.btnCasa.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.btnCasa.Name = "btnCasa";
+            this.btnCasa.Size = new System.Drawing.Size(23, 22);
+            this.btnCasa.Text = "toolStripButton1";
+            this.btnCasa.ToolTipText = "volver a la pagina principal";
+            this.btnCasa.Click += new System.EventHandler(this.btnCasa_Click);
+            // 
+            // btnBuscar
+            // 
+            this.btnBuscar.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.btnBuscar.Image = ((System.Drawing.Image)(resources.GetObject("btnBuscar.Image")));
+            this.btnBuscar.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.btnBuscar.Name = "btnBuscar";
+            this.btnBuscar.Size = new System.Drawing.Size(23, 22);
+            this.btnBuscar.Text = "toolStripButton1";
+            this.btnBuscar.ToolTipText = "buscar lo escrito";
+            this.btnBuscar.Click += new System.EventHandler(this.btnBuscar_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.ClientSize = new System.Drawing.Size(872, 450);
+            this.Controls.Add(this.webBrowser1);
+            this.Controls.Add(this.toolStrip1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "StrikeNet";
+            this.toolStrip1.ResumeLayout(false);
+            this.toolStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ToolStrip toolStrip1;
+        private System.Windows.Forms.ToolStripButton btnAtras;
+        private System.Windows.Forms.ToolStripButton btnAdelante;
+        private System.Windows.Forms.ToolStripButton btnActualizar;
+        private System.Windows.Forms.ToolStripButton btnParar;
+        private System.Windows.Forms.ToolStripTextBox txtUrl;
+        private System.Windows.Forms.WebBrowser webBrowser1;
+        private System.Windows.Forms.ToolStripButton btnCasa;
+        private System.Windows.Forms.ToolStripButton btnBuscar;
+    }
+}
+
---
+++ Navegador1/Navegador1/Form1.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Navegador1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnBuscar_Click(object sender, EventArgs e)
+        {
+            webBrowser1.Navigate(txtUrl.Text);
+        }
+
+        private void btnAtras_Click(object sender, EventArgs e)
+        {
+            webBrowser1.GoBack();
+        }
+
+        private void btnAdelante_Click(object sender, EventArgs e)
+        {
+            webBrowser1.GoForward();
+        }
+
+        private void btnActualizar_Click(object sender, EventArgs e)
+        {
+            webBrowser1.Refresh();
+
+        }
+
+        private void btnParar_Click(object sender, EventArgs e)
+        {
+            webBrowser1.Stop();
+        }
+
+        private void btnCasa_Click(object sender, EventArgs e)
+        {
+            webBrowser1.GoHome();
+        }
+    }
+}
